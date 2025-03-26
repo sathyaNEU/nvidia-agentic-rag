adf87219425af4359e420204823927aa804a90b7
sathyaNEU <144740003+sathyaNEU@users.noreply.github.com>
2025-03-26 02:49:46 -0400

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -3,9 +3,7 @@ import pandas as pd
 import requests
 import matplotlib.pyplot as plt
 
-BACKEND_URL = "https://agentic-rag-451496260635.us-central1.run.app"  # Adjust as needed
-# BACKEND_URL = "http://localhost:8000"
-
+BACKEND_URL = "https://agentic-rag-451496260635.us-central1.run.app"
 model_mapper = {
     "openai/gpt-4o": "gpt-4o-2024-08-06",
     "openai/gpt-3.5-turbo": "gpt-3.5-turbo-0125",
